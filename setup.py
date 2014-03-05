0f07c64a9603ac5867c2cbbed9e03e11f58086ca
Ian Good <ian.good@rackspace.com>
2014-03-05 17:14:53 -0500

    set version to 0.1.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='warren',
-      version='0.0.0',
+      version='0.1.0',
       author='Ian Good',
       author_email='icgood@gmail.com',
       description='Utility for managing a cluster of RabbitMQ nodes.',
